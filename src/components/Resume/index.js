4c1242574be9960709d4ebd4b3d973cc2488750b
aziz <ahmed613@umn.edu>
2022-01-19 16:37:46 -0600

    resume

---
+++ src/components/Resume/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+const Resume = () => {
+  return (
+    <main>
+      <h2 className="pb-3">Resume</h2>
+        <a href="">
+            Download Sample Resume
+        </a>
+        
+      <div class="justify-content-center mt-5">
+        <div>
+          <h2>Proficiencies</h2>
+          <ul>
+            <li>
+            HTML, CSS, JavaScript, jQuery, Node.js, Express.js, SQL, IndexedDB,<br />
+            MongoDB, NoSQL, React
+            </li>
+          </ul>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Resume;
