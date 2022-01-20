43523cd3b49da370ecbd768389abdf55712cb3d1
aziz <ahmed613@umn.edu>
2022-01-20 13:29:03 -0600

    done

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -8,12 +8,12 @@ const Projects = (props) => {
             {props.projectsArray.map(project => (
                 <div className="card col-5 px-2 py-2 my-2 mx-auto" key={project.id} >
                     <div className="card-header">
-                        <h3 className="bold card-title">{project.name}</h3>
+                        <h3 className="strong card-title">{project.name}</h3>
                     </div>
-                    <img class="card-img" src={project.image} alt="relevant-image-to-card" />
+                    <img class="card-img" src={project.image} alt="project screenshot" />
                     <div className="card-footer">
-                        <a className=" mx-2" href={project.liveSite}>Deployed</a>
-                        <a className="mx-2" href={project.repo}>Github Repo</a>
+                        <a className=" mx-2" href={project.liveSite}>Deployed Site</a>
+                        <a className="mx-2" href={project.repo}>Github Repository</a>
                     </div>
                 </div>
             ))}
