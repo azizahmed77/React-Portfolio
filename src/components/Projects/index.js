f7770b64fbf792f7b09a7b2006c4cd4a4b8e2af1
aziz <ahmed613@umn.edu>
2022-01-19 22:50:23 -0600

    portfolio looking better

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,21 +4,21 @@ import React from "react";
 
 const Projects = (props) => {
     return (
-      <div className >
-        {props.projectsArray.map(project => (
-        <div className="card px-2 py-2 my-2 mx-2 w-50" key={project.id} >
-        <div className="card-header">
-          <h3 className="bold card-title">{project.name}</h3>
+        <div className="row" >
+            {props.projectsArray.map(project => (
+                <div className="card col-5 px-2 py-2 my-2 mx-auto" key={project.id} >
+                    <div className="card-header">
+                        <h3 className="bold card-title">{project.name}</h3>
+                    </div>
+                    <img class="card-img" src={project.image} alt="relevant-image-to-card" />
+                    <div className="card-footer">
+                        <a className=" mx-2" href={project.liveSite}>Deployed</a>
+                        <a className="mx-2" href={project.repo}>Github Repo</a>
+                    </div>
+                </div>
+            ))}
         </div>
-        <img class="card-img" src={project.image} alt="relevant-image-to-card"/>
-        <div className="card-footer">
-            <a className=" mx-2" href={project.liveSite}>Deployed</a>
-            <a className="mx-2" href={project.repo}>Github Repo</a>
-          </div>
-        </div>
-        ))}
-      </div>
-      
+
     );
 }
 
