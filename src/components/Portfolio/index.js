b86773de60893cb47330e75b6d1e17df48b420c6
aziz <ahmed613@umn.edu>
2022-01-19 23:01:51 -0600

    project links working

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -15,37 +15,37 @@ const projectsArray = [
     id: 1,
     name: "Quote-Battle",
     image: "/kanye.png",
-    liveSite: "https://arcane-retreat-82910.herokuapp.com/home",
-    repo: "https://github.com/azizahmed77/baby-brain"
+    liveSite: "https://concord511.github.io/Quote-Battle/",
+    repo: "https://github.com/azizahmed77/Quote-Battle"
   },
   {
     id: 1,
     name: "Code Quiz",
     image: "/codequiz.png",
-    liveSite: "https://arcane-retreat-82910.herokuapp.com/home",
-    repo: "https://github.com/azizahmed77/baby-brain"
+    liveSite: "https://azizahmed77.github.io/Code-Quiz/",
+    repo: "https://github.com/azizahmed77/Code-Quiz"
   },
 
   {
     id: 1,
     name: "Weather Dashboard",
     image: "/weather.png",
-    liveSite: "https://arcane-retreat-82910.herokuapp.com/home",
-    repo: "https://github.com/azizahmed77/baby-brain"
+    liveSite: "https://azizahmed77.github.io/Weather-dashboard/",
+    repo: "https://github.com/azizahmed77/Weather-dashboard"
   },
   {
     id: 1,
     name: "Taskmaster Pro",
     image: "/taskmaster.png",
-    liveSite: "https://arcane-retreat-82910.herokuapp.com/home",
-    repo: "https://github.com/azizahmed77/baby-brain"
+    liveSite: "https://azizahmed77.github.io/taskmaster-pro/",
+    repo: "https://github.com/azizahmed77/taskmaster-pro"
   },
   {
     id: 1,
     name: "Run-Buddy",
     image: "/run-buddy.png",
-    liveSite: "https://arcane-retreat-82910.herokuapp.com/home",
-    repo: "https://github.com/azizahmed77/baby-brain"
+    liveSite: "https://azizahmed77.github.io/run-buddy/",
+    repo: "https://github.com/azizahmed77/run-buddy"
   }
 ];
 
