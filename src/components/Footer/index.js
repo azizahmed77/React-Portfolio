28225b09aaf49d5d44318ab2536a2bc1300ea5e6
aziz <ahmed613@umn.edu>
2022-03-30 13:26:18 -0500

    update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,8 @@ const Footer = () => {
     <footer>
         <a href="https://github.com/azizahmed77"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github"/></a>
         <a href="https://www.linkedin.com/in/aziz-ahmed"><img src="https://img.icons8.com/doodle/48/000000/linkedin--v2.png" alt="LinkedIn"/></a>
-        
+        <br></br>
+        <h6>ahmed613@umn.edu</h6>
     </footer>
   );
 }
