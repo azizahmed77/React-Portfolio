4a6cfcd2761b7affbc5e6c908c53a65f9d1e813e
aziz <ahmed613@umn.edu>
2022-03-30 13:13:02 -0500

    add project descriptions

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -13,6 +13,7 @@ const projectsArray = [
     id: 1,
     name: "Connect-Four",
     image: connectFour,
+    description: "Our rendition of a classic two-player connection board game",
     liveSite: "https://cbc-connect-4.herokuapp.com/",
     repo: "https://github.com/azizahmed77/connect-four"
   },
@@ -20,6 +21,8 @@ const projectsArray = [
     id: 1,
     name: "Deep Thoughts",
     image: deepThoughts,
+    description: ` A social media app where users can create an account, share thoughts and react to others’ thoughts.`
+    ,
     liveSite: "https://arcane-depths-10773.herokuapp.com/",
     repo: "https://github.com/azizahmed77/Deep-Thoughts"
   },
@@ -27,6 +30,8 @@ const projectsArray = [
     id: 1,
     name: "Shop-Shop",
     image: shopShop,
+    description: `An e-commerce application that allows a user to purchase an assortment of different products by adding them to a shopping cart and completing their order through a secure payment platform. A user is also able to view a history of their orders.
+    `,
     liveSite: "https://stark-beach-94885.herokuapp.com/",
     repo: "https://github.com/azizahmed77/shop-shop-redux"
   },
@@ -35,6 +40,7 @@ const projectsArray = [
     id: 1,
     name: "Weather Dashboard",
     image: weatherDashboard,
+    description: "An app that retrives the weather of a given location, allowing you to plan trips accordingly",
     liveSite: "https://azizahmed77.github.io/Weather-dashboard/",
     repo: "https://github.com/azizahmed77/Weather-dashboard"
   },
@@ -42,6 +48,7 @@ const projectsArray = [
     id: 1,
     name: "Taskmaster Pro",
     image: taskmaster,
+    description: " A note-taking app that will help to better oeganize your daily activiies",
     liveSite: "https://azizahmed77.github.io/taskmaster-pro/",
     repo: "https://github.com/azizahmed77/taskmaster-pro"
   },
@@ -49,6 +56,7 @@ const projectsArray = [
     id: 1,
     name: "Run-Buddy",
     image: runBuddy,
+    description: "A front-facing website for a gym to display to their clients",
     liveSite: "https://azizahmed77.github.io/run-buddy/",
     repo: "https://github.com/azizahmed77/run-buddy"
   }
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -9,6 +9,7 @@ const Projects = (props) => {
                 <div className="card col-5 px-2 py-2 my-2 mx-auto" key={project.id} >
                     <div className="card-header">
                         <h3 className="strong card-title">{project.name}</h3>
+                        <h6 className="card-text">{project.description}</h6>
                     </div>
                     <img class="card-img" src={project.image} alt="project screenshot" />
                     <div className="card-footer">
